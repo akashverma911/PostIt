b2b50d4c35007fe21344580909e84194e7d89b9d
Akash Verma <akash.911verma@gmail.com>
2021-12-09 19:43:44 +0530

    errors rectified

--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'groups'
+app_name = 'posts'
 
 urlpatterns = [
-    path('', views.ListGroups.as_view(), name="all"),
-    path("new/", views.CreateGroup.as_view(), name="create"),
-    path("posts/in/<slug>/",views.SingleGroup.as_view(),name="single"),
-    path("join/<slug>/",views.JoinGroup.as_view(),name="join"),
-    path("leave/<slug>/",views.LeaveGroup.as_view(),name="leave"),
+    path('', views.PostList.as_view(), name="all"),
+    path("new/", views.CreatePost.as_view(), name="create"),
+    path("by/<username>/", views.UserPosts.as_view(), name="for_user"),
+    path("by/<username>/<int:pk>/", views.PostDetail.as_view(), name="single"),
+    path("delete/<int:pk>/", views.DeletePost.as_view(), name="delete"),
 ]
