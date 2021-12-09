4da972d16a9ad4c5d81f13f8ad55669f3a557572
Akash Verma <akash.911verma@gmail.com>
2021-12-09 18:04:53 +0530

    errors

--- groups/models.py
+++ groups/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 from django import template
 register = template.Library()
 
-class Group(models.models):
+class Group(models.Model):
     name = models.CharField(max_length = 255,unique=True)
     slug = models.SlugField(allow_unicode=True,unique=True)
     description = models.TextField(blank=True,default='')
@@ -27,9 +27,9 @@ class Group(models.models):
     class Meta:
         ordering = ['name']
 
-class GroupMember(models.model):
-    user = models.ForeignKey(User,related_name='user_groups')
-    group = models.ForeignKey(Group,related_name="membership")
+class GroupMember(models.Model):
+    user = models.ForeignKey(User,related_name='user_groups',on_delete=models.CASCADE)
+    group = models.ForeignKey(Group,related_name="membership",on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
-class GroupMemberInline(admin.TabularInline):
-    model = model.GroupMember
 
-admin.site.register(models.Group)
+admin.site.register(models.Post)
